0fe98512639a2f99270e822387129ba1635f6b53
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-23 15:35:36 +0300

    init: init commit.

---
+++ lua/plugins/minimap.lua
@@ -0,0 +1,9 @@
+  return {
+    'wfxr/minimap.vim',
+    build = "cargo install --locked code-minimap",
+    config = function()
+      vim.cmd("let g:minimap_width = 20")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  }
