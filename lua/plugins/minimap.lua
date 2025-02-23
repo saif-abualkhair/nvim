91ce8caf620a601417be66c3e29bbdd4459673ba
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-02-23 22:40:51 +0300

    feat: added auto run for Minimap on init

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -5,5 +5,6 @@
       vim.cmd("let g:minimap_width = 20")
       vim.cmd("let g:minimap_auto_start = 1")
       vim.cmd("let g:minimap_auto_start_win_enter = 1")
+      vim.cmd("Minimap")
     end,
   }
